2dcc31fa94a3a7442c59ceaf89bc0f3d8ae783c6
Santiago <guzman.santiago135@gmail.com>
2024-06-05 19:46:03 -0300

    comentarios sacados

--- Cartelera/Cartelera/login/index.js
+++ Cartelera/Cartelera/login/index.js
@@ -38,17 +38,14 @@ var modal = document.getElementById("myModal");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("closeSuccess")[0];
 
-// When the user clicks the button, open the modal 
 function mostrarModal() {
     modal.style.display = "flex";
 }
 
-// When the user clicks on <span> (x), close the modal
 function ocultarModal() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
