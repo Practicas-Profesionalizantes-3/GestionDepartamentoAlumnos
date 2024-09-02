5c49208b324ed7d55d3443ca5bf21afb0fb4d9c9
MaximilianoLopez <31918166@itbeltran.com.ar>
2024-09-02 17:30:59 -0300

    principio de ticket

---
+++ tramites/js/ticket.js
@@ -0,0 +1,9 @@
+function abrirModal() {
+    var modal = document.getElementById("modal-ticket");
+    modal.style.display = "block";
+  }
+  
+  function cerrarModal() {
+    var modal = document.getElementById("modal-ticket");
+    modal.style.display = "none";
+  }
