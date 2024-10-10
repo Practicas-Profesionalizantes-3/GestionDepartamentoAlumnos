79e4dd1d0deba5496b33181d78fc0b2c85f71201
LucianoGomezITB <42950565@itbeltran.com.ar>
2024-10-10 18:02:03 -0300

    4

--- includes/cartelera.php
+++ includes/cartelera.php
@@ -37,92 +37,31 @@ if (isset($_SESSION['mostrar_opciones_cartelera'])) {
         <h2 class="mb-5"><span class="tm-text-primary">Cartelera de Alumnos - Noticias & Novedades</span></h2>
     </div>
 
-    <div class="buscador">
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <div class="d-flex justify-content-center align-items-center mb-5">
-                <div class="input-group" style="max-width: 600px;">
-                    <input class="form-control barra" type="text" id="search-input" name="search-input" placeholder="Buscar anuncio por título o descripción...">
-                    <button type="submit" name="buscar" class="btn btn-primary btn-primary-buscar">
-                        <i class="bi bi-search"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-    </div>
-
-
-
-    <?php
-        if (isset($_POST['buscar'])) {
-            $search_term = strtolower($_POST['search-input']); // Convertir el término de búsqueda a minúsculas
-            
-            $datos_filtrados = array_filter($datos, function ($item) use ($search_term) {
-                // Convertir los textos de 'titulo' y 'descripcion' a minúsculas para hacer la comparación
-                return strpos(strtolower($item['titulo']), $search_term) !== false || 
-                    strpos(strtolower($item['descripcion']), $search_term) !== false;
-            });
-            
-            $datos = $datos_filtrados;
-        }
-    ?>
-
-    
- <div class="row justify-content-center">
-    <?php if ($datos) : ?>
-        <?php foreach ($datos as $index => $item) : ?>
-            <div class="col-lg-4 tm-col-home mb-4" data-wow-delay="0.1s">
-                <div class="position-relative border-cartelera">
-                    <div class="img-cartelera mb-3">
-                        <img class="img-cartelera-item" src="<?= $item["imagen"] != "" ? "data:image/jpeg;base64," . $item["imagen"] : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQheiic81_IfFML2GH1T9qtee4KTajErPLBmg&s" ?>" />
-                    </div>
-                    <hr style="width: 50%;" class="d-flex m-auto">
-                    <div class="img-container mt-3">
-                        <div class="overlay">
-                            <div>
-                                <h5 class="mb-3 cartelera-titulo"><?php echo htmlspecialchars($item['titulo']); ?></h5>
-                                <p class="cartelera-desc text-truncate" id="desc-<?php echo $index; ?>">
-                                    <?php 
-                                        if (strlen($item['descripcion']) > 455) {
-                                            echo htmlspecialchars(substr($item['descripcion'], 0, 100)) . "...";
-                                        } else {
-                                            echo htmlspecialchars($item['descripcion']);
-                                        }
-                                    ?>
-                                </p>
-
-                                <!-- Botón Ver más solo si la descripción tiene más de 45 caracteres -->
-                                <?php if (strlen($item['descripcion']) > 45) : ?>
-                                    <form action="http://localhost/gestiondepartamentoalumnos/includes/aviso.php" method="POST">
-                                        <input type="hidden" name="id" value="<?= htmlspecialchars($item['id_aviso']); ?>">
-                                        <input type="hidden" name="titulo" value="<?= htmlspecialchars($item['titulo']); ?>">
-                                        <input type="hidden" name="descripcion" value="<?= htmlspecialchars($item['descripcion']); ?>">
-                                        <input type="hidden" name="imagen" value="<?= htmlspecialchars($item['imagen']); ?>">
-                                        <input type="hidden" name="fecha" value="<?= htmlspecialchars($item['fecha_publicacion']); ?>">
-                                        <input type="hidden" name="adjunto" value="<?= htmlspecialchars($item['adjunto']); ?>">
-                                        <button type="submit" class="btn btn-secondary">Ver más</button>
-                                    </form>
-                                <?php endif; ?>
-
-                                <!-- Contenedor para la fecha -->
-                                <div class="fecha-container">
-                                    <p><?php echo htmlspecialchars($item['fecha_publicacion']); ?></p>
-                                </div>
-                                <div class="descargar-adjunto">
-                                    <a href="data:application/pdf;base64,<?= $item["adjunto"]; ?>" download="<?= htmlspecialchars($item["titulo"]); ?>">Descargar adjunto</a>
+    <div class="row">
+        <?php if ($datos) : ?>
+            <?php foreach ($datos as $item) : ?>
+                <div class="col-lg-6 tm-col-home mb-4" data-wow-delay="0.1s">
+                    <div class="position-relative border-cartelera">
+                        <div class="img-cartelera">
+                            <img class="img-cartelera-item" src="<?= $item["imagen"] != "" ? "data:image/jpeg;base64," . $item["imagen"] : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQheiic81_IfFML2GH1T9qtee4KTajErPLBmg&s" ?>" />
+                        </div>
+                        <div class="img-container">
+                            <div class="overlay">
+                                <div>
+                                    <h5 class="mb-3 cartelera-titulo"><?php echo htmlspecialchars($item['titulo']); ?></h5>
+                                    <p class="cartelera-desc"><?php echo htmlspecialchars($item['descripcion']); ?></p>
+                                    <div class="fecha-container">
+                                        <p><?php echo htmlspecialchars($item['fecha_publicacion']) . "/" . htmlspecialchars($item['fecha_vencimiento']); ?></p>
+                                    </div>
+                                    <div class="descargar-adjunto">
+                                        <a href="data:application/pdf;base64,<?= $item["adjunto"]; ?>" download="<?= htmlspecialchars($item["titulo"]); ?>">Descargar adjunto</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        <?php endforeach; ?>
-    <?php endif; ?>
-</div> <!--Fin de cartelera.row-->
-
-
-
-
-
-
-
-
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div> <!--Fin de cartelera.row-->
+</section> <!--Fin de section-->
